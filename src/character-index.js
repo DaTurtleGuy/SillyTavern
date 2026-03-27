9b505e87c6ffb197391e2e4ffe7cbbd7e791d5ba
Turtle Guy <168043062+DaTurtleGuy@users.noreply.github.com>
2026-03-27 00:22:18 -0300

    fixed shit so it runs on android... i think

--- src/character-index.js
+++ src/character-index.js
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import fs from 'node:fs';
-import Database from 'better-sqlite3';
+import { DatabaseSync } from 'node:sqlite';
 
 const INDEX_VERSION = 1;
 
@@ -17,8 +17,8 @@ function getDb() {
     if (!fs.existsSync(dbDir)) {
         fs.mkdirSync(dbDir, { recursive: true });
     }
-    db = new Database(dbPath);
-    db.pragma('journal_mode = WAL');
+    db = new DatabaseSync(dbPath);
+    db.exec('PRAGMA journal_mode = WAL');
     initTables(db);
     return db;
 }
